c7d7369a46276f579ddf6035b0acc96314fca8c5
Geoffrey <59753500+Geoffrey2002@users.noreply.github.com>
2021-06-18 14:01:39 +0300

    Adding wrapper component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
-
+import Wrapper from '../Helpers/Wrapper'
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
@@ -44,7 +44,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
